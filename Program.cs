1b1465fd76421d8f9ee69494b09097b6da9eb473
Pan Prezes <Pan Prezes@PREZES>
2022-03-15 18:58:09 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace QuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+
+            Console.WriteLine("Quess the number from 1 to 100\nTry to do it in 5 steps or less");
+
+            int howMuch = 0;
+            int myNumber;
+
+            do
+            {
+
+                howMuch++;
+                Console.Write("Give me the number:");
+                myNumber = int.Parse(Console.ReadLine());
+                if (myNumber < 1 || myNumber > 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" Your number is incorrect! Choose between 1 to 100\n Try again!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    if (myNumber > randomNumber)
+                    {
+                        Console.WriteLine("The number is too high!");
+                    }
+                    else if (myNumber < randomNumber)
+                    {
+                        Console.WriteLine("The number is too low!");
+                    }
+                    else
+                    {
+                        if (howMuch <= 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($" Correct number!!!!\n You need {howMuch} steps to win!!\n PERFECT!!");
+
+                        }
+                        else if (howMuch <= 10)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($" Correct number!\n You need {howMuch} steps to win!\n GOOD!");
+
+                        }
+
+                        else 
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($" Correct number\n But you need {howMuch} steps to win :( its to much :(\n Try again !");
+
+                        }
+                    }
+                }
+            } while (myNumber != randomNumber);
+
+            Console.ResetColor();
+            Console.ReadKey();
+
+        }
+    }
+}
